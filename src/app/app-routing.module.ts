cff8f65ea874d03f8b781e75d3a83055b9b5e15b
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-05-15 09:32:54 -0600

    inital commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'shop', component: ShopComponent},
+  {path: 'about', component: AboutComponent}, 
+  {path: '', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
